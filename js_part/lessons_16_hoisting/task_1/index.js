d33d21f15686a930692403bfcd7c97aea4300b71
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-06 15:42:07 +0300

    commit

--- js_part/lessons_16_hoisting/task_1/index.js
+++ js_part/lessons_16_hoisting/task_1/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
-let a = 17;
+// let a = 17;
 
-if (a) {
-  let a = 1;
+// if (a) {
+//   let a = 1;
 
-  console.log(a);
-}
+//   console.log(a);
+// }
 
-console.log(a);
+// console.log(a);
+
+
