6cb32bda50dc59246778d2cfb85a6d92eb276473
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-08 16:22:19 +0300

    less 21

---
+++ js_part/lessons_21-DOM/task_2/index.js
@@ -0,0 +1,15 @@
+export function getTitleElement () {
+  const element = document.querySelector('.title');
+  console.dir(element);
+  return element;
+}
+
+getTitleElement();
+
+export function InputElement() {
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem);
+  return inputElem;
+}
+
+InputElement();
