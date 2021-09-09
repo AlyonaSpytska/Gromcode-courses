92034653d13ff760aecf51d3b1b0b6df2b1b7bce
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-09 21:15:08 +0300

    task_12, take 2

--- js_part/lessons_21-DOM/task_12/index.js
+++ js_part/lessons_21-DOM/task_12/index.js
@@ -1,6 +1,7 @@
-export const squareNumbers = () => {
-  // eslint-disable-next-line array-callback-return
-  const element = Array.from(document.querySelectorAll('.number')).map(li => li.dataset.squaredNumber = li.dataset.number ** 2); 
+export const squaredNumbers = () => {
+  const element = Array.from(document.querySelectorAll('.number')).map(
+    li => (li.dataset.squaredNumber = li.dataset.number ** 2),
+  );
 };
 
-squareNumbers();
+squaredNumbers();
