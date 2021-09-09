e8bd1b05277ecb72ba02fa923265c6f19ee8b7cd
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-09 18:27:33 +0300

    1

---
+++ js_part/lessons_21-DOM/task_10/index.js
@@ -0,0 +1,9 @@
+export const finishForm = () => {
+  const changeInputType = document.querySelector('input');
+  changeInputType.setAttribute('type', 'password')
+  const formElem = document.querySelector('.login-form');
+  const getInput = document.createElement('input');
+  getInput.setAttribute('name', 'login');
+  formElem.prepend(getInput);
+}
+
