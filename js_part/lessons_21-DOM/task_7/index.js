7cd875ab35ee3efc7fce16ff44109a59cb8c6c18
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-09 16:15:39 +0300

    task_7, take 2

--- js_part/lessons_21-DOM/task_7/index.js
+++ js_part/lessons_21-DOM/task_7/index.js
@@ -1,4 +1,4 @@
-const clearList = () => {
+export const clearList = () => {
   // eslint-disable-next-line no-multi-assign
   const cleanList = (document.querySelector('.categories').innerHTML = ``);
 };
