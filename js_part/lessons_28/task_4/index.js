7e5c89b9a98805772e5c9594a4139cf27e1a7663
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-20 13:10:18 +0300

    task 4 take 1

--- js_part/lessons_28/task_4/index.js
+++ js_part/lessons_28/task_4/index.js
@@ -27,7 +27,7 @@ const add2 = value => value + 2;
 const square = value => value * value;
 const half = value => value / 2;
 
-const compose =
+export const compose =
   (...funcs) =>
   value =>
     funcs.reduce((acc, func) => func(acc), value);
