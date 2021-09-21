030f7148ebc1ad486493f101ca7f537f4efbe403
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-21 17:32:18 +0300

    added files for les 31, made 1 and 2 tasks

--- js_part/lessons_31_handlers&chaining/task_2/index.js
+++ js_part/lessons_31_handlers&chaining/task_2/index.js
@@ -25,32 +25,26 @@
 // };
 
 export const asyncCalculator = num =>
-  new Promise(resolve => {
+  new Promise((resolve) => {
     setTimeout(() => {
       console.log(`Initial value: ${num}`);
       resolve(num);
     }, 500);
+  }).then(
+    value =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          const result = value * value;
+          console.log(`Square value: ${result}`);
+          resolve(result);
+        }, 1000);
+      }),
+  )
+  .then(value => {
+    const result = value * 2;
+    console.log(`Doubled value: ${result}`);
+    return result;
   })
-    .then(
-      value =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            const result = value * value;
-            console.log(`Square value: ${result}`);
-            resolve(result);
-          }, 500);
-        }),
-    )
-    .then(
-      value =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            const result = value * 2;
-            console.log(`Doubled value: ${result}`);
-            resolve(result);
-          }, 500);
-        }),
-    );
 
 
   asyncCalculator(5)
