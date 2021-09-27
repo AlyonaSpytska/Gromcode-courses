1721033cd6d27754f775d0fbe3209fcdf94c29b5
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-27 16:14:34 +0300

    made task_2, take 1, take 1

--- js_part/lessons_34_http_part_2/task_2/index.js
+++ js_part/lessons_34_http_part_2/task_2/index.js
@@ -1,14 +1,16 @@
-const baseUrl = 'https://6151b9d34a5f22001701d3b4.mockapi.io/users';
+const baseUrl = 'https://6151c2184a5f22001701d3f3.mockapi.io/api/v1/users';
 
-export function getUsersList() {
+function getUsersList() {
   return fetch(baseUrl).then(response => response.json());
 }
 
-export function getUserById(userId) {
-  return fetch(`${baseUrl}/${userId}`).then(response => response.json());
+function getUserById(userId) {
+  return fetch(`${baseUrl}/${userId}`).then(response =>
+    response.json(),
+  );
 }
 
-export function createUser(userData) {
+function createUser(userData) {
   return fetch(baseUrl, {
     method: 'POST',
     headers: {
@@ -18,13 +20,13 @@ export function createUser(userData) {
   });
 }
 
-export function deleteUser(userId) {
+function deleteUser(userId) {
   return fetch(`${baseUrl}/${userId}`, {
     method: 'DELETE',
   });
 }
 
-export function updateUser(userId, userData) {
+function updateUser(userId, userData) {
   return fetch(`${baseUrl}/${userId}`, {
     method: 'PUT',
     headers: {
