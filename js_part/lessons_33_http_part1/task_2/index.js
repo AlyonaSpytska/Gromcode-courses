00e6d164aa177b8dfeea5f5ce74c4efa2158f8b1
Alyona Spytska <mshelenspytska@gmail.com>
2021-09-27 13:45:43 +0300

    added return for getTasksList func

--- js_part/lessons_33_http_part1/task_2/index.js
+++ js_part/lessons_33_http_part1/task_2/index.js
@@ -1,7 +1,7 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 export function getTasksList() {
-  fetch(baseUrl).then(arrayTasks => arrayTasks.json());
+  return fetch(baseUrl).then(arrayTasks => arrayTasks.json());
 }
 
 export function getTaskById(taskId) {
