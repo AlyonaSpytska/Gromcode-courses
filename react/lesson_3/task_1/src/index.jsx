3e10b88e38d366ef61407ab3775fae619007d249
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-05 14:44:57 +0300

    commit task_1

---
+++ react/lesson_3/task_1/src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import Search from "Search.jsx";
+import "./index.scss";
+import "./search.scss";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Search name="Tom" />, rootElem);
