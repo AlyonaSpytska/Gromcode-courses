56b6838f6f4c37a65098bfb6b16fc883448ec942
SaraFilin <84840548+SaraFilin@users.noreply.github.com>
2021-10-05 14:48:26 +0300

    Delete react/lesson_3/task_1 directory

--- react/lesson_3/task_1/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import Search from "Search.jsx";
-import "./index.scss";
-import "./search.scss";
-
-const rootElem = document.querySelector("#root");
-
-ReactDOM.render(<Search name="Tom" />, rootElem);
