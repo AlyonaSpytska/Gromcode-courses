364f234306011f87b1a925ee3e4fa06240526014
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-06 11:33:50 +0300

    made task_1

--- react/lesson_4/task_1/src/App.jsx
+++ react/lesson_4/task_1/src/App.jsx
@@ -5,8 +5,8 @@ const App = () => {
   return (
     <>
       <Counter start={5} interval={1000} />
-      <Counter start={12} interval={1500} />
-      <Counter start={-21} interval={500} />
+      <Counter start={12} interval={2000} />
+      <Counter start={-21} interval={3000} />
     </>
   );
 };
