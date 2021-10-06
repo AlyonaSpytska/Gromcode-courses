f0aeb513050fae4337828cae743bbeb8a40c346c
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-06 12:55:55 +0300

    made task_2, take 1

--- react/lesson_4/task_1/src/index.jsx
+++ react/lesson_4/task_1/src/index.jsx
@@ -1,9 +1,8 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import "./index.scss";
-import App from './App'
+import App from "./App";
 
-const rootElem = document.querySelector('#root')
+const rootElem = document.querySelector("#root");
 
-ReactDOM.render(<App />, rootElem)
-
+ReactDOM.render(<App />, rootElem);
