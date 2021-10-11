6f32600855dc08da9c94f5f0d602afe850c3a5df
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-11 19:43:54 +0300

    task 1, Rendering list, map method for li

---
+++ react/lesson_7/task_1/src/NumbersList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NumbersList = ({ numbers }) => {
+  return (
+    <ul>
+      {numbers.map(number => (
+        <li key={number}>{number}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default NumbersList;
--- react/lesson_7/task_1/src/index.jsx
+++ react/lesson_7/task_1/src/index.jsx
@@ -2,9 +2,12 @@ import ReactDOM from "react-dom";
 import React from "react";
 
 import "./index.scss";
-import NumberList from "./NumberList";
+import NumbersList from "./NumbersList";
 const numbers = [1, 2, 3, 4, 5]
 
 
 
-ReactDOM.render(<NumberList numbers={numbers} />, document.querySelector("#root"));
+ReactDOM.render(
+  <NumbersList numbers={numbers} />,
+  document.querySelector("#root")
+);
