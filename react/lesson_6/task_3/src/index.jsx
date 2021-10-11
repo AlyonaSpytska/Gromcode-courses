6fc6a5fe6f6f8fb3c6bb7132db17f685e4f70855
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-11 16:07:47 +0300

    made task 3, cancel rendering

---
+++ react/lesson_6/task_3/src/index.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+import "./index.scss";
+import Page from "./Page.jsx";
+
+const rootElem = document.querySelector("#root");
+ReactDOM.render(<Page />, rootElem);
