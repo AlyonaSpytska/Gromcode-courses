a30cc60bbb7c24537140ec03891d4e12e2a23154
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-11 16:14:00 +0300

    made task 3, cancel rendering

--- react/lesson_6/task_3/src/Page.jsx
+++ react/lesson_6/task_3/src/Page.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Message from "./Message.jsx";
 
 class Page extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      text: null,
-    };
-  }
+  //   this.state = {
+  //     text: null,
+  //   };
+  // }
+  state = {
+    text: null,
+  };
 
   setText = text => {
     this.setState({
@@ -25,7 +28,10 @@ class Page extends React.Component {
           <button className="btn" onClick={() => this.setText("Hello, world!")}>
             Text 1
           </button>
-          <button className="btn" onClick={() => this.setText('Another exciting text.')}>
+          <button
+            className="btn"
+            onClick={() => this.setText("Another exciting text.")}
+          >
             Text 2
           </button>
           <button className="btn" onClick={() => this.setText("")}>
