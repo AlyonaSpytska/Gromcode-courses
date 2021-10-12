68293098f9d421ab7e6dbfac496ec4d7012ca836
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-12 18:39:23 +0300

    Методы жизненного цикла, task_1

--- react/lesson_8/task_1/src/Life.jsx
+++ react/lesson_8/task_1/src/Life.jsx
@@ -20,7 +20,7 @@ class Life extends React.Component {
     console.log(
       "shouldComponentUpdate(nextProps, nextState): decide to render or not to render"
     );
-    return nextProps.number % 2;
+    return true;
   }
 
   componentDidUndate(prevProps, prevState) {
