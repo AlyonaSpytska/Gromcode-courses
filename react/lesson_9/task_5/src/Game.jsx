8e037558aa0244b52de8780f5e9884695be756b3
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-18 18:19:17 +0300

    Tic-tac-toe game

--- react/lesson_9/task_5/src/Game.jsx
+++ react/lesson_9/task_5/src/Game.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import Board from './Board'
+import React from "react";
+import Board from "./Board.jsx";
 
 function calculateWinner(squares) {
   const lines = [
@@ -21,48 +21,79 @@ function calculateWinner(squares) {
   return null;
 }
 
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
 
-const Game = () => {
-    const [board, setBoard] = useState(Array(9).fill(null))
-    const [xIsNext, setXIsNext] = useState(true)
-    const winner = calculateWinner(board)
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
 
-    const handleClick = (index) => {
-        const boardCopy = [...board]
-        // Определить был ли клик по ячейке или игра законченна
-        if (winner || boardCopy[index]) return
-        // Определить чей ход Х ? О
-        boardCopy[index] = xIsNext ? 'X' : 'O'
-        // Обновить state
-        setBoard(boardCopy)
-        setXIsNext(!xIsNext)
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
 
-    const startNewGame = () => {
-      return (
-        <button
-          className="btn__start"
-          onClick={() => setBoard(Array(9).fill(null))}
-        >
-          {" "}
-          Очистить поле{" "}
-        </button>
-      );
-    };
-
-
-
     return (
-      <div className="wrapper">
-        {startNewGame()}
-        <Board squares={board} click={handleClick} />
-        <p className="game-info">
-          {winner
-            ? "Победитель " + winner
-            : "Сейчас ходит " + (xIsNext ? "X" : "O")}
-        </p>
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
       </div>
     );
+  }
 }
 
-export default Game
+export default Game;
