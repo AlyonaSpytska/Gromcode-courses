dc5b9c12acce021173e89f8aa4ae626e9807ab0f
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-19 15:25:52 +0300

    Список товаров, task_2

---
+++ react/lesson_10/task_2/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ShoppingCart from "./ShoppingCart";
+import Profile from "./Profile";
+
+class Page extends React.Component {
+  state = {
+    userData: {
+      firstName: "Sara",
+      lastName: "Filin",
+    },
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      userData: {
+        ...this.state.userData,
+        [name]: value,
+      },
+    });
+  };
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <div className="page">
+        <h1 className="title">
+          {`Hello, ${userData.firstName} ${userData.lastName}`}
+        </h1>
+        <main className="content">
+          <ShoppingCart 
+          userData={userData} />
+          <Profile 
+          userData={userData} 
+          handleChange={this.handleChange} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Page;
