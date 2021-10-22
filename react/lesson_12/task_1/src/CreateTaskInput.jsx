036984d06e5fdf9ae26b5b9c019a74efe930b084
Alyona Spytska <mshelenspytska@gmail.com>
2021-10-22 18:04:20 +0300

    Todo List. practice, fetch to mock API

--- react/lesson_12/task_1/src/CreateTaskInput.jsx
+++ react/lesson_12/task_1/src/CreateTaskInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class CreateTaskInput extends React.Component {
   state = {
@@ -11,29 +12,34 @@ class CreateTaskInput extends React.Component {
     });
   };
 
-  handleTaksCreate = () => {
+  handleTaskCreate = () => {
     this.props.onCreate(this.state.value);
-    this.setState({
-      value: ''
-    })
-  }
+    this.setState({ value: "" });
+  };
 
   render() {
     return (
       <div className="create-task">
         <input
           type="text"
-          className="create-task__input"
           value={this.state.value}
           onChange={this.handleChange}
+          className="create-task__input"
         />
-        <button className="btn" onClick={this.handleTaksCreate}>
+        <button
+          className="btn create-task__btn"
+          onClick={this.handleTaskCreate}
+        >
           Create
         </button>
       </div>
     );
   }
 }
+
+CreateTaskInput.propTypes = {
+  onCreate: PropTypes.func.isRequired,
+};
 
 export default CreateTaskInput;
 
