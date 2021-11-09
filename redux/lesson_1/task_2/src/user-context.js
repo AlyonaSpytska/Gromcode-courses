6c78862e48342b9a27290000c7542c0feba06a64
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-09 15:34:43 +0200

    User menu with React Context

--- redux/lesson_1/task_2/src/user-context.js
+++ redux/lesson_1/task_2/src/user-context.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
 export const userInfo = {
-    name: "Nicola Tesla",
-    avatar_url: "https://avatars3.githubusercontent.com/u10001",
+  name: "Nikola Tesla",
+  avatar_url: "https://avatars3.githubusercontent.com/u10001",
 };
 
 export const userDataContext = React.createContext(userInfo);
