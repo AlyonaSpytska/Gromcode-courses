22451fb59b44908a1f1deef4981bbe05b9550850
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-11 14:07:03 +0200

    redux-dev-tools

--- redux/lesson_3/task_2/src/index.js
+++ redux/lesson_3/task_2/src/index.js
@@ -1,9 +1,8 @@
-import store from "./counter";
-import { increment, decrement, reset } from "./counter";
+import { store, increment, decrement } from "./counter";
 
 store.subscribe(() => console.log(store.getState()));
 
-// store.dispatch(increment());
-// store.dispatch(increment());
-// store.dispatch(decrement());
-// store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
