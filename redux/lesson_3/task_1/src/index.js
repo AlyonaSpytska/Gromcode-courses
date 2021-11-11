0b0c648c986bae39d58719557fd3dac37009d0b0
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-11 13:45:21 +0200

    Комбинирование редьюсеров

--- redux/lesson_3/task_1/src/index.js
+++ redux/lesson_3/task_1/src/index.js
@@ -4,14 +4,14 @@ import { addUser, deleteUser, updateUser } from "./users.actions";
 
 store.subscribe(() => console.log(store.getState()))
 
-// store.dispatch(increment())
-// store.dispatch(increment());
-// store.dispatch(decrement());
-// store.dispatch(increment());
-// store.dispatch(reset());
+store.dispatch(increment())
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(reset());
 
-// store.dispatch(addUser({id: 1, name: 'Tom'}));
-// store.dispatch(addUser({ id: 2, name: "Bob" }));
+store.dispatch(addUser({id: 1, name: 'Tom'}));
+store.dispatch(addUser({ id: 2, name: "Bob" }));
 
-// store.dispatch(deleteUser(2));
-// store.dispatch(updateUser(1, {name: 'Sara'}));
+store.dispatch(deleteUser(2));
+store.dispatch(updateUser(1, {name: 'Sara'}));
