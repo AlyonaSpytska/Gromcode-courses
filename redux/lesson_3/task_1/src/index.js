3d0615dd38938daf6dd3b9b5eee82c425a0c05f7
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-11 13:40:26 +0200

    Комбинирование редьюсеров

--- redux/lesson_3/task_1/src/index.js
+++ redux/lesson_3/task_1/src/index.js
@@ -1,5 +1,5 @@
 import store from './store'
-import {increment, decrement, reset} from './counter.actions'
+import { increment, decrement, reset } from "./counter.actions";
 import { addUser, deleteUser, updateUser } from "./users.actions";
 
 store.subscribe(() => console.log(store.getState()))
