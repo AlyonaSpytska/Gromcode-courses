f19c4df561150322a1356b884dc355eb70dec770
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-19 11:31:09 +0200

    Weather app - React + Redux

--- redux/lesson_6/task_2/src/users/users.gateway.js
+++ redux/lesson_6/task_2/src/users/users.gateway.js
@@ -1,6 +1,6 @@
 const baseUrl = "https://api.github.com/users";
 
-export const getUserData = userName =>
+export const getWeatherDatas = userName =>
   fetch(`${baseUrl}/${userName}`).then(response => {
     if (response.ok) {
       return response.json();
