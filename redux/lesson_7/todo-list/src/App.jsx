55e29bd09130eb90429c254e8fcc695589d56575
Alyona Spytska <mshelenspytska@gmail.com>
2021-11-21 21:37:28 +0200

    redux, lesson 7. Practice, todo-list, added redux functional

--- redux/lesson_7/todo-list/src/App.jsx
+++ redux/lesson_7/todo-list/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import TodoList from "./TodoList";
+import { Provider } from "react-redux";
+import store from "./store";
+import TodoList from "./tasks/TodoList";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <TodoList />
-    </>
+    </Provider>
   );
 };
 
